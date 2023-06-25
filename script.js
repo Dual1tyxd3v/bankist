85351bca77fe9dc4b1e8eb4633808414ad60d76c
B3rzxd3v <d3vilc0re@gmail.com>
2023-06-25 22:40:42 +0700

    Add date function

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 // BANKIST APP
 
 // Data
-const account1 = {
+/* const account1 = {
   owner: 'Jonas Schmedtmann',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
@@ -33,8 +33,48 @@ const account4 = {
   pin: 4444,
 };
 
-const accounts = [account1, account2, account3, account4];
+const accounts = [account1, account2, account3, account4]; */
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
 
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
+};
+
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
+};
+
+const accounts = [account1, account2];
 // Elements
 const labelWelcome = document.querySelector('.welcome');
 const labelDate = document.querySelector('.date');
@@ -61,16 +101,25 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = (movs, sort = false) => {
+const getHumanDate = (date) => ({
+  day: date.getDate().toString().padStart(2, '0'),
+  month: (date.getMonth() + 1).toString().padStart(2, '0'),
+  year: date.getFullYear(),
+  hours: date.getHours().toString().padStart(2, '0'),
+  minutes: date.getMinutes().toString().padStart(2, '0'),
+});
+
+const displayMovements = ({movements: movs, movementsDates}, sort = false) => {
   const movements = sort ? movs.slice().sort((a, b) => a - b) : movs;
 
   containerMovements.innerHTML = '';
   movements.forEach((move, i) => {
+    const {day, month, year} = getHumanDate(new Date(movementsDates[i]));
     const type = move > 0 ? 'deposit' : 'withdrawal';
     const html = `
     <div class="movements__row">
       <div class="movements__type movements__type--${type}">${i} ${type}</div>
-      <div class="movements__date">3 days ago</div>
+      <div class="movements__date">${day}/${month}/${year}</div>
       <div class="movements__value">${move.toFixed(2)}€</div>
     </div>`;
     containerMovements.insertAdjacentHTML('afterbegin', html);
@@ -103,8 +152,11 @@ const displayTotalStat = ({movements, interestRate}) => {
 let currentUser;
 
 const updateUI = (acc) => {
+  const {day, month, year, hours, minutes} = getHumanDate(new Date());
+  labelDate.textContent = `As of ${day}/${month}/${year} ${hours}:${minutes}`;
+
   displayDeposit(acc);
-  displayMovements(acc.movements);
+  displayMovements(acc);
   displayTotalStat(acc);
 }
 
@@ -135,6 +187,8 @@ btnTransfer.addEventListener('click', (e) => {
 
   currentUser.movements.push(-amount);
   targetUser.movements.push(amount);
+  currentUser.movementsDates.push(new Date().toISOString());
+  targetUser.movementsDates.push(new Date().toISOString());
   inputTransferAmount.value = inputTransferTo.value = '';
   inputTransferAmount.blur();
 
@@ -160,6 +214,7 @@ btnLoan.addEventListener('click', (e) => {
   if (amount <= 0 || !currentUser.movements.some(mov => mov >= amount * 0.1)) return;
 
   currentUser.movements.push(amount);
+  currentUser.movementsDates.push(new Date().toISOString());
   inputLoanAmount.value = '';
   updateUI(currentUser);
 });
@@ -167,6 +222,6 @@ btnLoan.addEventListener('click', (e) => {
 let sorted = false;
 
 btnSort.addEventListener('click', () => {
-  displayMovements(currentUser.movements, !sorted);
+  displayMovements(currentUser, !sorted);
   sorted = !sorted;
 });
